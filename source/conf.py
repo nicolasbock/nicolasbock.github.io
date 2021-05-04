c9e09cb0f5c6837966ec35d005c5bb4c39ec04b4
Nicolas Bock <nicolasbock@gmail.com>
2021-05-04 13:35:17 -0600

    Cleanup
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -28,10 +28,7 @@ author = 'Nicolas Bock'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.googleanalytics',
 ]
-
-googleanalytics_id = 'UA-107410166-1'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
